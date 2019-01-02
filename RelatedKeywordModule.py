ca3990d571911c48184485bbc73e25b9fcdb22a0
Mingyu Jo <owlsogul@naver.com>
2019-01-02 15:55:56 +0900

    Create RelatedKeywordModule.py

---
+++ RelatedKeywordModule.py
@@ -0,0 +1,42 @@
+# 사용하는 라이브러리
+# requests
+# bs4
+
+# 초기에 입력받은 여러개의 키워드의 연관 검색어를 조사해서
+# 키워드 수를 늘려주는 모듈입니다.
+
+from bs4 import BeautifulSoup
+import urllib.request
+from urllib  import parse
+
+def findRelatedKeywordsOfOne(keyword):
+    founded_keywords = []
+    base_url = "https://search.naver.com/search.naver?&query="
+    url = base_url + parse.quote(keyword)
+
+    html = urllib.request.urlopen(url)
+    doc = html.read()
+    soup = BeautifulSoup(doc, "html.parser")
+
+    attributes = {"class" : "_related_keyword_ul"}
+    soup_related_keyword_ul = soup.find("ul", attributes)
+    soup_lis = []
+    if soup_related_keyword_ul is not None:
+        soup_lis = soup_related_keyword_ul.select("li")
+
+    for li in soup_lis :
+        keyword = li.find("a").string
+        founded_keywords.append(keyword)
+    return founded_keywords
+
+def findRelatedKeywordsOfMany(keywords):
+    result_keywords = []
+    for keyword in keywords:
+        print("==== " + keyword + " 연관 검색어 결과 ====")
+        temp_keywords = findRelatedKeywordsOfOne(keyword)
+        print(temp_keywords)
+        result_keywords += temp_keywords
+    return result_keywords
+
+init_keywords = ["트와이스", "다현"]
+print(findRelatedKeywordsOfMany(init_keywords))
