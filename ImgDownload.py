6ae649a3b7d645fc532da66b36dc7a66cbde3831
sshrik <chj878194@naver.com>
2019-01-07 09:16:08 +0900

    주석 처리 완료 및 사용 예시 작성

--- ImgDownload.py
+++ ImgDownload.py
@@ -21,22 +21,50 @@ def downloadImg(link, dest, fileName):
     urllib.request.urlretrieve(link, dest + "/" + fileName)
     return True
 
-def showLink(link, dec="utf-8"):
+def getLink(link, dec="utf-8"):
     '''
+    이미지 링크를 수집.
+    ARGS:
+        - link : html 다운로드 받을 이미지가 있는 링크.
+        - dec : decoding Type ( 한글은 기본 utf-8 )
+    RAISE:
+    RETURN:
+        - imgLink : 이미지 다운받을 링크의 배열.
     '''
     html = urllib.request.urlopen(link).read().decode(dec)
     token = divToken(html)
-    for l in token:
-        if "img" in l or "image" in l:
-            if "src=" in l:
-                print(l.split("src=\"")[1].split("\"")[0])
+    imgLink = []
+    for t in token:
+        if "img" in t or "image" in t and "src=" in t:
+                imgLink.append(divSrc(t))
+    return imgLink
 
 def divToken(html):
     '''
+    html 에서 <> 기준으로 토큰 분리.
+    ARGS:
+        - html : html source
+    RAISE:
+    RETURN:
+        - token : <> 기준으로 나뉜 토큰들.
     '''
     token = html.split('<')
     for i in range(0, len(token)):
         token[i] = token[i].split('>')[0]
     return token
 
-showLink("http://pguin.tistory.com/559?category=639235")
+def divSrc(token):
+    '''
+    src="" 기준으로 토큰 분리.
+    ARGS:
+        - token : HTML Source 에서 <> 기준으로 나뉜 토큰 값들.
+    RAISE:
+    RETURN:
+        - src : token 에서 "src=...." 으로 된 값들의 real value.
+    '''
+    return token.split("src=\"")[1].split("\"")[0]
+
+# 사용 예시
+imgLink = getLink("http://pguin.tistory.com/559?category=639235")
+for i in imgLink:
+    print(i)
